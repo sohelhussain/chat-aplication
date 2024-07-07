e2006ed9aab2605f586130f786453c38f962fa00
sohelhussain <sohelhussaing@gmail.com>
2024-07-07 07:04:35 +0530

    commplite authentification code

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -28,3 +28,42 @@ module.exports.userCreationsController = (req, res, next) => {
     });
   })
 };
+
+// user login get
+
+module.exports.userLoginPageController = (req, res, next) => {
+  res.render("login");
+};
+
+
+// user login post
+
+module.exports.userLoginController = async (req, res, next) => {
+  let {email, password} = req.body;
+  let user = await userModel.findOne({email});
+  if (!user) res.status(404).send({message: "user not found"});
+  bcrypt.compare(password, user.password, (err, result) => {
+    if(result){
+      let token = jwt.sign({email: user.email, id: user._id}, process.env.JWT_PASSKEY);
+      res.cookie('token', token);
+      res.send('user are loggedin');
+    }else{
+      res.send('your password are incorrect');
+    }
+  })
+};
+
+
+//user logout
+
+module.exports.userLogoutController = (req, res, next) => {
+  res.cookie('token', '');
+  res.redirect('/login');
+};
+
+
+//user dashboard
+
+module.exports.userDashboardController = (req, res, next) => {
+  res.render('dashboard');
+};
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,13 @@
 const app = require("express");
 const router = app.Router();
-const { lendingPageController, userCreationsController } = require("../controllers/index-controller");
+const { lendingPageController, userCreationsController, userDashboardController, userLoginPageController, userLoginController, userLogoutController } = require("../controllers/index-controller");
 
 router.get("/", lendingPageController);
 router.post("/register", userCreationsController);
+router.get("/login", userLoginPageController);
+router.post("/login", userLoginController);
+router.get("/logout", userLogoutController);
+
+router.get("/dashboard", userDashboardController);
 
 module.exports = router;
